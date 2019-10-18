f5d71d0ab30588ec217c57e2b01a7a60ee7e49c3
eliorcohen123 <44778837+eliorcohen123@users.noreply.github.com>
2019-10-18 16:11:22 +0300

    Update AsmPro.asm

--- AsmPro.asm
+++ AsmPro.asm
@@ -149,10 +149,10 @@ _exit:
    mov   ecx, largest
    mov   edx, 2
    mov   ebx, 1      ;file descriptor (stdout)
-   mov   eax, 4.     ;system call number (sys_write)
+   mov   eax, 4      ;system call number (sys_write)
    int   0x80        ;call kernel
     
-   mov   eax, 1.     ;system call number (sys_exit)
+   mov   eax, 1      ;system call number (sys_exit)
    int   0x80        ;call kernel
    
 exit:
